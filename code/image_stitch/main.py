59c76aa7f7aa54e2d9a1d4e0c86272588e214e98
Mason Reyher <mr.noitall@yahoo.com>
2023-04-11 09:08:38 -0600

    add feature matching functionality

--- code/image_stitch/main.py
+++ code/image_stitch/main.py
@@ -4,38 +4,56 @@ import helper
 
 '''Inspiration for this stitching algorithm taken from github.com/lukasalexanderweber'''
 
-imgs = ['../../data/test1.jpg', '../../data/test2.jpg']
+imgs = ['../../data/donkey_split_left.jpg', '../../data/dokey_split_mid.jpg','../../data/dokey_split_right.jpg']
 for i, x in enumerate(imgs):
     imgs[i] = cv.imread(x)
 while True:
 
+
+    # resize
+    for i, x in enumerate(imgs):
+        imgs[i] = cv.resize(x, (640, 480), interpolation=cv.INTER_LINEAR_EXACT)
+
+    # find minutia
+
+    orb = cv.ORB.create()
+
+    features = []
+    minutia = []
+    feature_imgs = []
+    for i, x in enumerate(imgs):
+        features.append(cv.detail.computeImageFeatures2(orb, x))
+        minutia.append(features[i].getKeypoints())
+        feature_imgs.append(cv.drawKeypoints(x, minutia[i], None, color=(0,0,255)))
+
+    # match minutia to each image
+    matcher = cv.detail_AffineBestOf2NearestMatcher()
+    '''for i, x in enumerate(imgs):
+        matches = matcher.apply2(features[i])
+        match_matrix = matches.array_in_square_matrix(matches)
+    confidence = [[m.confidence for m in row] for row in match_matrix]
+    print(confidence)'''
+
+    # subsetting
+
+    # estimate camera parameters
+
+    # warping
+
+    # timelapsing
+
+    # crop
+
+    # seam masking
+
+    # exposure correction
+
+    # blending
+
+
+
     for i, img in enumerate(imgs):
-        # resize
-        img = cv.resize(img, (640, 480), interpolation=cv.INTER_LINEAR_EXACT)
-
-        # find minutia
-
-        # match minutia to each image
-
-        # subsetting
-
-        # estimate camera parameters
-
-        # warping
-
-        # timelapsing
-
-        # crop
-
-        # seam masking
-
-        # exposure correction
-
-        # blending
-
-
-
-        cv.imshow(f'image-{i}', img)
+        cv.imshow(f'image-{i}', feature_imgs[i])
 
 
 
